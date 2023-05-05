93ef56ab2b22268f94d18f08ada0e63ff89f29d6
vcamargo04 <viniciusac2005@hotmail.com>
2023-05-05 09:23:56 -0300

    Declaração de Var/Let/Const.

---
+++ js/script.js
@@ -0,0 +1,61 @@
+   // //Declarar uma variável com a palavra reservada var.
+   // var nr1 = "10";
+   // var nr2 = "5";
+
+   // //Vamos imprimir as variáveis com o objeto console.
+   // console.log(nr1);
+   // console.log(nr2);
+
+   // //Realizando as operações básicas com estes valores:
+   // // + / - / * e /
+
+   
+   // //REALIZE A CONCATENAÇÃO COM O TEXTO RESULTADO:
+   // //EX: console.log(RESULTADO nr1 + nr2);
+   
+   // //ADIÇÃO
+   // console.log("RESULTADO ERRADO " + (nr1 + nr2));
+   // console.log("RESULTADO ADIÇÃO: " +(parseInt (nr1) + parseInt (nr2)));
+   // console.log(`RESULTADO ADIÇÃO:  ${parseInt(nr1) + parseInt(nr2)} `);
+   // //SUBTRAÇÃO
+   // console.log(`RESULTADO SUBTRAÇÃO :  ${parseInt(nr1) - parseInt(nr2)} `);
+
+   // //MULTIPLICAÇÃO
+   // console.log(`RESULTADO MULTIPLICAÇÃO:  ${parseInt(nr1) * parseInt(nr2)} `);
+
+   // //DIVISÃO
+   // console.log(`RESULTADO DIVISÃO:  ${parseInt(nr1) / parseInt(nr2)} `);
+
+   // //OBTENHA O RESTO DA DIVISÃO
+   // console.log(`RESTO DA DIVISÃO:  ${parseInt(nr1) % parseInt(nr2)} `);
+
+   //DESCUBRINDO SE UM NÚMERO É PAR OU IMPAR
+   //UTILIZANDO O OPERADOR TERNÁRIO(?).
+//    var nr1 = "10";
+//    var nr2 = "5";
+//    var resultado;
+
+//    resultado = !(parseInt(nr1) % parseInt(nr2)) ? "PAR" : "IMPAR";
+// console.log(`O NÚMERO É  : ${resultado}`);
+
+
+//REGRAS DO CONST
+//1º Regra = Inicializar na declaração
+// const nome3 = "Mario";
+//2ª Regra = Não pode ser alterado durante o fluxo do programa.
+// nome = "Luan";
+
+// const PI = 3.14
+
+//DIFERENÇA ENTRE VAR/LET/CONST
+// var nome1 = "Joao";
+// let nome2 = "Maria";
+
+// let nome = "Beatriz";
+
+// if(nome != ""){
+//    let nome = "Robson"
+   
+// }
+// //Qual é o nome que será impresso no log?
+// console.log(nome)
